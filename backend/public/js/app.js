6a3a326f58ec5356a2c5d0fa639dbfbbbbb44f1c
moririn3464 <moririn3464@gmail.com>
2021-08-23 16:26:52 +0900

    react-router-dom導入、ディレクトリ構成一部変更

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -6774,7 +6774,7 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
  */
 
 
-__webpack_require__(/*! ./components/Example */ "./resources/js/components/Example.js");
+__webpack_require__(/*! ./pages/Example */ "./resources/js/pages/Example.js");
 
 /***/ }),
 
@@ -6822,10 +6822,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/js/components/Example.js":
-/*!********************************************!*\
-  !*** ./resources/js/components/Example.js ***!
-  \********************************************/
+/***/ "./resources/js/pages/Example.js":
+/*!***************************************!*\
+  !*** ./resources/js/pages/Example.js ***!
+  \***************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -6857,7 +6857,7 @@ function Example() {
             children: "Example Component"
           }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
             className: "card-body",
-            children: "I'm an example component!"
+            children: "pages\u30C7\u30A3\u30EC\u30AF\u30C8\u30EA\u306B\u79FB\u52D5"
           }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_3__.default, {
             color: "primary",
             variant: "contained",
--- backend/resources/js/pages/Example.js
+++ backend/resources/js/pages/Example.js
@@ -10,7 +10,7 @@ function Example() {
                     <div className="card">
                         <div className="card-header">Example Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">pagesディレクトリに移動</div>
                         
                         <Button color="primary" variant="contained">Hello World</Button>
                     </div>
